3653d1920ce82bd061860e0e17d8bbfa77db146e
AUVINET Matys <matysauvinet@gmail.com>
2024-04-05 15:23:25 +0200

    Modif API LOGIN

--- Api/loginAPI.php
+++ Api/loginAPI.php
@@ -1,22 +1,35 @@
 <?php
 
+global $pdo;
 require_once 'ConnAPI.php';
 require_once 'GenerateToken.php';
 
 $email = $_POST["email"];
 $password = $_POST["password"];
+
+$token = GenerateToken();
+
 $requete = "SELECT * FROM user WHERE email = ?";
 $stmt = $pdo->prepare($requete);
 $stmt->execute([$email]);
 $utilisateur = $stmt->fetch();
+
 if ($utilisateur && password_verify($password, $utilisateur['mot_de_passe'])) {
 
     $requete2 = "UPDATE user SET token = ? WHERE id_user = ?";
     $stmt2 = $pdo->prepare($requete2);
     $stmt2->execute([$token, $utilisateur['id_user']]);
 
-    unset($utilisateur['mot_de_passe'], $utilisateur['token']);
-    $json = array("status" => 200, 'message' => "Success", "user" => $utilisateur);
+    if (isset($utilisateur['mot_de_passe'])) {
+        unset($utilisateur['mot_de_passe']);
+    }
+    if (isset($utilisateur['token'])) {
+        unset($utilisateur['token']);
+    }
+
+    $json = array("status" => 200, 'message' => "Success");
+    $json = array_merge($json, $utilisateur);
+    $json['token'] = $token;
 } else {
     $json = array("status" => 400, 'message' => "Error");
 }
